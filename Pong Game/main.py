79a96778b35adb077d170bca46e4b30c6243d8c0
Jean Claude Coppola <104370725+C-JeanDev@users.noreply.github.com>
2022-12-28 13:37:36 +0100

    Update main.py

--- Pong Game/main.py
+++ Pong Game/main.py
@@ -17,11 +17,10 @@ class Player():
         self.vel = 5
 
     def draw(self) -> None:
-        self.command()
         pygame.draw.rect(
             self.win, WHITE, (self.x, self.y, self.width, self.height))
 
-    def command(self) -> None:
+    def command(self, keys) -> None:
         if self.mode == 1:
             if keys[pygame.K_w] and self.y > 0:
                 self.y -= self.vel
@@ -32,6 +31,10 @@ class Player():
                 self.y -= self.vel
             if keys[pygame.K_DOWN] and self.y < win_height - self.height:
                 self.y += self.vel
+
+    def update(self, keys):
+        self.command(keys)
+        self.draw()
 
 
 class Ball:
@@ -49,10 +52,10 @@ class Ball:
         if not (self.check()):
             self.x: int = win_width//2
             self.y: int = win_height//2
-        
+
         if self.x == win_width//2 and self.y == win_height//2:
-            ball.y_vel = 0        
-            
+            self.y_vel = 0
+
         pygame.draw.circle(win, WHITE, (self.x, self.y), self.radius)
 
     def move(self):
@@ -66,97 +69,100 @@ class Ball:
             return False
         return True
 
-    def point_system(self, win):
-        # se la posizione della palla e' maggiore di win_width il punto va al player di sinistra
-        # se la posizione della palla e' minore di 0 il punto va al player di destra
-        global left_point
-        global right_point
-        if self.x > win_width:
-            left_point += 1
-        elif self.x < 0:
-            right_point += 1
+    def update(self, win):
+        self.draw(win)
+        self.move()
+
+
+class Game:
+
+    def __init__(self, width: int, height: int):
+        self.win_width, self.win_height = width, height
+        self.WIN_SIZE: tuple = (self.win_width, self.win_height)
+        self.win = pygame.display.set_mode(self.WIN_SIZE)
+
+        self.player1 = Player(self.win, 10, self.win_width // 2 - 100 * 1.5, 1)
+        self.player2 = Player(
+            self.win, 785, self.win_width // 2 - 100 * 1.5, 2)
+
+        self.ball = Ball(self.win_width//2, self.win_height//2, 7)
+
+        self.left_point = 0
+        self.right_point = 0
+
+    def point_system(self):
+
+        if self.ball.x > win_width:
+            self.left_point += 1
+        elif self.ball.x < 0:
+            self.right_point += 1
 
         fnt = pygame.font.SysFont("Arial", 40, bold=True)
 
-        t1 = fnt.render(str(left_point), True, "white")
-        t2 = fnt.render(str(right_point), True, "white")
+        t1 = fnt.render(str(self.left_point), True, "white")
+        t2 = fnt.render(str(self.right_point), True, "white")
 
-        win.blit(t1, (200, 0))
-        win.blit(t2, (600, 0))
+        self.win.blit(t1, (200, 0))
+        self.win.blit(t2, (600, 0))
 
+    def collision(self):
+        if self.ball.y + self.ball.radius >= self.win_height:
+            self.ball.y_vel *= -1
 
-def collision(ball, player1, player2):
-    if ball.y + ball.radius >= win_height:
-        ball.y_vel *= -1
+        elif self.ball.y - self.ball.radius <= 0:
+            self.ball.y_vel *= -1
 
-    elif ball.y - ball.radius <= 0:
-        ball.y_vel *= -1
+        if self.ball.x_vel < 0:
+            if self.ball.y >= self.player1.y and self.ball.y <= self.player1.y + self.player1.height:
+                if self.ball.x - self.ball.radius <= self.player1.x + self.player1.width:
+                    self.ball.x_vel *= -1
 
-    if ball.x_vel < 0:
-        if ball.y >= player1.y and ball.y <= player1.y + player1.height:
-            if ball.x - ball.radius <= player1.x + player1.width:
-                ball.x_vel *= -1
+                    middle_y = self.player1.y + self.player1.height / 2
+                    difference_in_y = middle_y - self.ball.y
+                    reduction_factor = (
+                        self.player1.height / 2) / self.ball.MAX_VEL
+                    y_vel = difference_in_y / reduction_factor
+                    self.ball.y_vel = -1 * y_vel
 
-                middle_y = player1.y + player1.height / 2
-                difference_in_y = middle_y - ball.y
-                reduction_factor = (player1.height / 2) / ball.MAX_VEL
-                y_vel = difference_in_y / reduction_factor
-                ball.y_vel = -1 * y_vel
+        else:
+            if self.ball.y >= self.player2.y and self.ball.y <= self.player2.y + self.player2.height:
+                if self.ball.x + self.ball.radius >= self.player2.x:
+                    self.ball.x_vel *= -1
 
-    else:
-        if ball.y >= player2.y and ball.y <= player2.y + player2.height:
-            if ball.x + ball.radius >= player2.x:
-                ball.x_vel *= -1
+                    middle_y = self.player2.y + self.player2.height / 2
+                    difference_in_y = middle_y - self.ball.y
+                    reduction_factor = (
+                        self.player2.height / 2) / self.ball.MAX_VEL
+                    y_vel = difference_in_y / reduction_factor
+                    self.ball.y_vel = -1 * y_vel
 
-                middle_y = player2.y + player2.height / 2
-                difference_in_y = middle_y - ball.y
-                reduction_factor = (player2.height / 2) / ball.MAX_VEL
-                y_vel = difference_in_y / reduction_factor
-                ball.y_vel = -1 * y_vel
+    def render(self):
+        pygame.init()
+        run: bool = True
+        clock = pygame.time.Clock()
 
+        while run:
+            self.win.fill((0, 0, 0))
+            keys = pygame.key.get_pressed()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
 
-def main() -> None:
+            clock.tick(80)
 
-    run: bool = True
+            self.player1.update(keys)
+            self.player2.update(keys)
 
-    while run:
-        win.fill((0, 0, 0))
-        global keys
-        keys = pygame.key.get_pressed()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        clock.tick(80)
-        player1.draw()
-        player2.draw()
-        ball.draw(win)
-        ball.move()
-        ball.point_system(win)
-        collision(ball, player1, player2)
-        pygame.display.update()
+            self.ball.update(self.win)
+
+            self.point_system()
+            self.collision()
+
+            pygame.display.update()
 
 
 if __name__ == "__main__":
-
-    left_point: int = 0
-    right_point: int = 0
-
-    # Color
     WHITE: tuple = (255, 255, 255)
-
-    pygame.init()
-
-    keys = None
-
     win_width, win_height = 800, 600
-    WIN_SIZE: tuple = (win_width, win_height)
-
-    clock = pygame.time.Clock()
-
-    win = pygame.display.set_mode(WIN_SIZE)
-
-    player1 = Player(win, 10, win_width // 2 - 100 * 1.5, 1)
-    player2 = Player(win, 785, win_width // 2 - 100 * 1.5, 2)
-    ball = Ball(win_width//2, win_height//2, 7)
-    ball1 = Ball(0, 0, 15)
-    main()
+    g = Game(win_width, win_height)
+    g.render()
